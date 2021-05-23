967ea42c9e918a2344406cb290cc1029420c3975
revzz01 <53479756+revzz01@users.noreply.github.com>
2021-05-23 15:25:46 +0530

    Add files via upload

---
+++ VNPD2021_KGCE_Q1/VNPD2021_KGCE_Q1.py
@@ -0,0 +1,34 @@
+def printArraystring(string, n):
+    for i in range(n):
+        print(string[i], end = " ")
+ 
+def sort(s, n):
+    for i in range(1, n):
+        temp = s[i]
+ 
+        
+        j = i - 1
+        while j >= 0 and len(temp) < len(s[j]):
+            s[j + 1] = s[j]
+            j -= 1
+ 
+        s[j + 1] = temp
+
+        
+        result = min(arr, key = len)
+        print("Shortest string is : " + res)
+
+                
+if __name__ == "__main__":
+    n = int(input("Enter no. of string in the array: "))
+    if n > 0:
+        arr = input()
+        for i in range(n):
+            s = input()
+            arr.append(s)
+    else:
+        print("Invalid Input")
+ 
+    sort(arr, n)
+ 
+    printArraystring(arr, n)
