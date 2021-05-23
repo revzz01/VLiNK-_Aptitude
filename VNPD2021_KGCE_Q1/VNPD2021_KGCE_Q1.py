fb51d6ff5f830734f77b711617bcb0447a0b37b2
revzz01 <53479756+revzz01@users.noreply.github.com>
2021-05-23 16:25:00 +0530

    Update VNPD2021_KGCE_Q1.py

--- VNPD2021_KGCE_Q1/VNPD2021_KGCE_Q1.py
+++ VNPD2021_KGCE_Q1/VNPD2021_KGCE_Q1.py
@@ -1,12 +1,13 @@
+#Print Array
 def printArraystring(string, n):
     for i in range(n):
         print(string[i], end = " ")
- 
+        
+#Sort the array in ascending order
 def sort(s, n):
     for i in range(1, n):
         temp = s[i]
  
-        
         j = i - 1
         while j >= 0 and len(temp) < len(s[j]):
             s[j + 1] = s[j]
@@ -18,7 +19,7 @@ def sort(s, n):
         result = min(arr, key = len)
         print("Shortest string is : " + res)
 
-                
+#Driver Code              
 if __name__ == "__main__":
     n = int(input("Enter no. of string in the array: "))
     if n > 0:
